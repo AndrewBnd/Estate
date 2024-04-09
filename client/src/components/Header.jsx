2b050a12571fa6ccc88503164d946c13adce28e7
AndrewBnd <andrey.bondarenko307@gmail.com>
2024-04-09 19:01:20 +0300

    sign up functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,28 +7,25 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
         <Link to="/">
           <h1 className="font-bold text-2xl flex">
-            <span>Estate</span>
+            <span>BestEstate</span>
           </h1>
         </Link>
-        <form className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
+        <form className="flex items-center border border-gray-300 rounded-xl py-2 px-4 shadow-md shadow-gray-300">
           <input
             type="text"
             placeholder="Поиск..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <button className="bg-sky-500 p-2 text-white rounded-full">
+          <button className="bg-blue-500 p-2 text-white rounded-full">
             <FaSearch />
           </button>
         </form>
-        <ul className="flex gap-4">
-          <Link to="/">
-            <li className="hidden sm:inline hover:underline">Home</li>
-          </Link>
+        <ul className="flex gap-6">
           <Link to="/about">
-            <li className="hidden sm:inline hover:underline">About</li>
+            <li className="hidden sm:inline hover:underline text-lg">О нас</li>
           </Link>
           <Link to="/sign-in">
-            <li className="hover:underline">Sign in</li>
+            <li className="hover:underline text-lg">Войти</li>
           </Link>
         </ul>
       </div>
