a64d940e34a688e33f8249abc80ce5d20da94e03
AndrewBnd <andrey.bondarenko307@gmail.com>
2024-04-16 16:29:46 +0300

    sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from 'react-redux';
 import { Link } from "react-router-dom";
@@ -103,6 +106,21 @@ export default function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(data.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Профиль</h1>
@@ -166,7 +184,7 @@ export default function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span  className="text-red-700 cursor-pointer">Выйти</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Выйти</span>
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Удалить аккаунт</span>
       </div>
       <p className='text-green-700 mt-5 text-center'>
